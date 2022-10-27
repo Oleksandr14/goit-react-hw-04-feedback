3f665d66f72fdc0c3348ed75ef27d9f1d78a806c
Oleksandr Palamarchuk <palamarchuk.oa93@gmail.com>
2022-10-27 12:44:07 +0300

    refactored App.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
-import { Component } from 'react';
+import { useState } from 'react';
+
 import { GlobalStyle } from '../styles/GlobalStyle';
 
 import { Statistics } from './Statistics/Statistics';
@@ -11,57 +12,64 @@ import { Notification } from './Notification/Notification';
 
 import { Box } from './Box';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const leaveFeedback = option => {
+    switch (option) {
+      case 'good':
+        setGood(p => p + 1);
+        break;
+      case 'neutral':
+        setNeutral(p => p + 1);
+        break;
+      case 'bad':
+        setBad(p => p + 1);
+        break;
+      default:
+        return;
+    }
   };
 
-  leaveFeedback = option => {
-    this.setState(prev => {
-      return { [option]: prev[option] + 1 };
-    });
-  };
+  const totalFeedback = good + neutral + bad;
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const totalFeedback = good + neutral + bad;
-    const positiveFeedback = Math.round((good * 100) / totalFeedback);
-    return (
-      <div>
-        <Box
-          width={480}
-          ml="auto"
-          mr="auto"
-          boxShadow={
-            '0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14), 0px 2px 1px rgba(0, 0, 0, 0.2)'
-          }
-        >
-          <Section>
-            <FeedbackOptions
-              title="Please leave feedback"
-              options={Object.keys(this.state)}
-              onLeaveFeedback={this.leaveFeedback}
+  const positiveFeedback = Math.round((good * 100) / totalFeedback);
+
+  return (
+    <div>
+      <Box
+        width={480}
+        ml="auto"
+        mr="auto"
+        boxShadow={
+          '0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14), 0px 2px 1px rgba(0, 0, 0, 0.2)'
+        }
+      >
+        <Section>
+          <FeedbackOptions
+            title="Please leave feedback"
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={leaveFeedback}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          {totalFeedback ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={totalFeedback}
+              positivePercentage={positiveFeedback ? positiveFeedback : 0}
             />
-          </Section>
-
-          <Section title="Statistics">
-            {totalFeedback ? (
-              <Statistics
-                good={good}
-                neutral={neutral}
-                bad={bad}
-                total={totalFeedback}
-                positivePercentage={positiveFeedback ? positiveFeedback : 0}
-              />
-            ) : (
-              <Notification message="There is no feedback" />
-            )}
-          </Section>
-        </Box>
-        <GlobalStyle />
-      </div>
-    );
-  }
-}
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
+      </Box>
+      <GlobalStyle />
+    </div>
+  );
+};
